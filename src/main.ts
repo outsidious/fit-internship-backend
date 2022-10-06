9d0311ae5a9044d0502280a492205769e265ba8f
Alexandr Kozachenko <Kozachenko.AA@gazprom-neft.ru>
2022-10-06 18:32:03 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
 
   const config = new DocumentBuilder()
         .setTitle("fil-meetups")
